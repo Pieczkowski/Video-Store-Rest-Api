415c1d4cdb82c439c02ac09c0351d46992c82664
Polikkox <damian.targosz11@gmail.com>
2018-11-28 09:29:08 +0100

    Add maven Spring boot JPA Thymeleaf PostgresSQL

---
+++ src/main/java/com/codecool/VideoStoreRestApi/VideoStoreRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.VideoStoreRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoStoreRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoStoreRestApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/codecool/VideoStoreRestApi/VideoStoreRestApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codecool.VideoStoreRestApi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VideoStoreRestApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
