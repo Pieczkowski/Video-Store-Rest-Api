243ced8f30ad7980424eeb83d096ba493c8f589d
Pieczkowski <pieczkowski.t@gmail.com>
2018-11-28 14:08:43 +0100

    Add GenreController

---
+++ src/main/java/com/codecool/VideoStoreRestApi/controllers/GenreController.java
@@ -0,0 +1,18 @@
+package com.codecool.VideoStoreRestApi.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/store/genre")
+public class GenreController {
+
+    private GenreService genreService;
+
+    @Autowired
+    GenreController(GenreService genreService){
+        this.genreService = genreService;
+    }
+
+}
