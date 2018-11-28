68e083a9f2448783e93d53099f2ce06a03101497
Pieczkowski <pieczkowski.t@gmail.com>
2018-11-28 12:04:25 +0100

    Add getAllMovies

--- src/main/java/com/codecool/VideoStoreRestApi/controllers/MovieController.java
+++ src/main/java/com/codecool/VideoStoreRestApi/controllers/MovieController.java
@@ -1,6 +1,8 @@
 package com.codecool.VideoStoreRestApi.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -13,5 +15,11 @@ public class MovieController {
     MovieController(MovieService movieService){
         this.movieService = movieService;
     }
+
+    @RequestMapping(value = "/store/movies", method = RequestMethod.GET)
+    public Collection<Movie> getAllMovies(){
+        return movieService.getAll();
+    }
+
 
 }
