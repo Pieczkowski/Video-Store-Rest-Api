07361a65456506d55cdea3e2cfc8039e51a063e4
ziutekk <aszmidt92@gmail.com>
2018-11-29 13:11:25 +0100

    Rename method

--- src/main/java/com/codecool/VideoStoreRestApi/repositories/MovieRepository.java
+++ src/main/java/com/codecool/VideoStoreRestApi/repositories/MovieRepository.java
@@ -1,12 +1,10 @@
 package com.codecool.VideoStoreRestApi.repositories;
 
-import com.codecool.VideoStoreRestApi.model.Director;
 import com.codecool.VideoStoreRestApi.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
-
-    Collection<Movie> findAllByDirectorIs(Director director);
+    Collection<Movie> findAllByDirectors(Long id_director);
 }
