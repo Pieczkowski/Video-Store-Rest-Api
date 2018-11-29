ca26c75809cfa8758268025864132de636decdcc
ziutekk <aszmidt92@gmail.com>
2018-11-29 11:32:45 +0100

    Add findAllByDirectorIs method

--- src/main/java/com/codecool/VideoStoreRestApi/repositories/MovieRepository.java
+++ src/main/java/com/codecool/VideoStoreRestApi/repositories/MovieRepository.java
@@ -1,7 +1,12 @@
 package com.codecool.VideoStoreRestApi.repositories;
 
+import com.codecool.VideoStoreRestApi.model.Director;
 import com.codecool.VideoStoreRestApi.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    Collection<Movie> findAllByDirectorIs(Director director);
 }
