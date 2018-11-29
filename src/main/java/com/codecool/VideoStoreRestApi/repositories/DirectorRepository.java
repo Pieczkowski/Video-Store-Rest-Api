6f7e3472142e1de3de967446f63579e7bafd6b32
Pieczkowski <pieczkowski.t@gmail.com>
2018-11-29 09:10:27 +0100

    Change from CrudRepository to JpaRepository

--- src/main/java/com/codecool/VideoStoreRestApi/repositories/DirectorRepository.java
+++ src/main/java/com/codecool/VideoStoreRestApi/repositories/DirectorRepository.java
@@ -3,7 +3,7 @@ package com.codecool.VideoStoreRestApi.repositories;
 import com.codecool.VideoStoreRestApi.model.Director;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DirectorRepository extends CrudRepository<Director, Long> {
+public interface DirectorRepository extends JpaRepository<Director, Long> {
 
 //    Collection<Movie> getMoviesByDirectorId(int id_director);
 }
