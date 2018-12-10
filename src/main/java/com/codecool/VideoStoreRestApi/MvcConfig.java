7622158be5722d34f490b55ef7eea50cee3fa1b5
ziutekk <aszmidt92@gmail.com>
2018-12-10 15:42:42 +0100

    Delete ClassLogger class and add MvcCOnfig class

---
+++ src/main/java/com/codecool/VideoStoreRestApi/MvcConfig.java
@@ -0,0 +1,15 @@
+package com.codecool.VideoStoreRestApi;
+
+import com.codecool.VideoStoreRestApi.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public abstract class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor());
+    }
+}
