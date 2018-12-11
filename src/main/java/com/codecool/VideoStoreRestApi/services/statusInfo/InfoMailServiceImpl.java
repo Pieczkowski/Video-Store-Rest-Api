382a98bd8dca1da72343f208e8a125f05acfa3ac
Pieczkowski <pieczkowski.t@gmail.com>
2018-12-11 02:30:50 +0100

    Add sender (from) in mail message

--- src/main/java/com/codecool/VideoStoreRestApi/services/statusInfo/InfoMailServiceImpl.java
+++ src/main/java/com/codecool/VideoStoreRestApi/services/statusInfo/InfoMailServiceImpl.java
@@ -1,6 +1,7 @@
 package com.codecool.VideoStoreRestApi.services.statusInfo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class InfoMailServiceImpl implements InfoMailService {
 
+    @Qualifier("getMailSender")
     @Autowired
     MailSender sender;
 
@@ -20,6 +22,7 @@ public class InfoMailServiceImpl implements InfoMailService {
             message.setTo(to);
             message.setSubject(subject);
             message.setText(text);
+            message.setFrom("gowniak1@spoko.pl");
 
             sender.send(message);
         } catch (MailException e){
